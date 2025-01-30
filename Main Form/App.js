37ad1824c3efcf32b2719407e015edf3ceda5cea
Ravikant <ravichirawa1421@gmail.com>
2025-01-30 11:01:02 +0530

    first commit

---
+++ Main Form/App.js
@@ -0,0 +1,50 @@
+
+
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     let form = document.getElementById("form");
+//     let button = document.getElementById("apply_button");
+//     let alert = document.getElementById("alert");
+//     let fileInput = document.getElementById("file_cv");
+//     button.addEventListener("click", (event) => {
+//         event.preventDefault();
+//         let formData = new FormData(form);
+//         let data = {};
+//         formData.forEach((val, key) => {
+//             data[key] = val;
+//         })
+//         let isCompleteData = Object.values(data).every((val)=>{
+//             return val != "" && val !=null && val !=undefined
+//         })
+//         const file = fileInput.files[0];
+//         if (file && isCompleteData) {
+//             const reader = new FileReader();
+//             reader.readAsDataURL(file);
+//             reader.onload = function () {
+//                 const fileData = {
+//                     name: file.name,
+//                     type: file.type,
+//                     content: reader.result,
+//                 };
+//                 localStorage.setItem("uploaded_file", JSON.stringify(fileData));
+//                 localStorage.setItem("form_data", JSON.stringify(data));
+//                 window.location.href = "../Submited/submited.html"
+//             };
+//         }
+//         else {
+//             alert.style.visibility = "visible"
+//         }
+//     });
+// });
+
+
+
+
+let a= 10;
+function fun(){
+    console.log(a);
+}
+fun(),
+(function(){
+    console.log(a);
+})()
---
+++ Response/response.js
@@ -0,0 +1,42 @@
+console.log(localStorage.getItem("form_data"));
+document.addEventListener("DOMContentLoaded", () => {
+    let firstNameField = document.getElementById("first_name");
+    let lastNameField = document.getElementById("last_name");
+    let emailField = document.getElementById("email");
+    let phoneNoField = document.getElementById("phone_no");
+    let jobRoleField = document.getElementById("job_role");
+    let dateField = document.getElementById("date");
+    let cityField = document.getElementById("city");
+    let pincodeField = document.getElementById("pincode");
+    let addressField = document.getElementById("address");
+    let anchor = document.getElementById("cv_link");
+
+    let data = JSON.parse(localStorage.getItem("form_data"))
+    const storedFile = localStorage.getItem("uploaded_file");
+
+    firstNameField.value = data.first_name;
+    lastNameField.value = data.last_name;
+    emailField.value = data.email;
+    phoneNoField.value = data.phone_no;
+    jobRoleField.value = data.job_role;
+    dateField.value = data.date;
+    cityField.value = data.city;
+    pincodeField.value = data.pincode;
+    addressField.value = data.address;
+
+    const fileData = JSON.parse(storedFile);
+    const { name, type, content } = fileData;
+
+
+        // // Create a download link
+        // const downloadLink = document.createElement("a");
+        // downloadLink.href = content; // Base64 data
+        // downloadLink.download = name;
+        // downloadLink.textContent = "Download File";
+        // document.body.appendChild(downloadLink);
+
+    anchor.href = content; 
+    anchor.download = name; 
+    anchor.innerHTML = name
+
+})
